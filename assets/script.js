31329583df2c920c51e4fa805c57cb2a406805a8
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-03-23 21:28:23 +1100

    Adding folder sturcture assets with scrtipt.js and stlye.css

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// Declaration of variables
+
+// Create the search function 
+
+// show the history of the search and be able to return to that search on the main div
+
+// show current search on main div
+
+// Show city name, the date an icon representing the weather conditions, the temperature, the humidity the wind speed, and the UV index.
+
+// When checking the UV create a color that indicates whether the conditions are favorable, moderate, or severe
+
+// view future weather conditions for that city and show a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+
+// Ensure when opening the weather dashboard, it opens with the last shown city.
+
+// optional - create a switch that can choose a random city wupon opening. 
