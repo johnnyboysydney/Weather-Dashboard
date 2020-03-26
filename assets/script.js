26d82fe3524e99f2798609b0a49f560864111107
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-03-26 18:39:23 +1100

    updating javscript.js

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,27 @@
 // Declaration of variables
-var dateTime = "";
+let dateTime = "";
 
 
 // show the history of the search and be able to return to that search on the main div
 $("#search").on("click", function() {
     event.preventDefault();
-    var city = $("#city-input").val().trim();
-    localStorage.setItem("city", city);
-    console.log(localStorage.getItem("city"));
-    searchCity(city);
-    forecast(city);
+    let city = $("#city-input").val().trim();
+    if(city != ''){
+        localStorage.setItem("city", city);
+        console.log(localStorage.getItem("city"));
+        searchCity(city);
+        forecast(city);
+    }
+    else {
+        $("#error").html("Field cannot be empty");
+    }
+
 });
 
-// Create the search function 
+// Creating the search function 
 function searchCity(city){
     
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=3d16044a2eba4d271046d70fd1f2c155";
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=3d16044a2eba4d271046d70fd1f2c155";
     
 $.ajax({
     url: queryURL,
@@ -24,24 +30,24 @@ $.ajax({
     console.log(response);
     // Setting variables for the city weather results 
     // show current search on main div
-    var cityName =$("#cityName").text(city);
-    var tempT = $("#temperature");
-    var humidity = $("#humidity").text("Humidity: "+ response.main.humidity + "%.");
-    var windSpeed = $("#windSpeed").text("WindSpeed: " + response.wind.speed + " m/s,");
-    var lon = response.coord.lon;
-    var lat = response.coord.lat;
+    let cityName =$("#cityName").text(city);
+    let tempT = $("#temperature");
+    let humidity = $("#humidity").text("Humidity: "+ response.main.humidity + "%.");
+    let windSpeed = $("#windSpeed").text("WindSpeed: " + response.wind.speed + " m/s,");
+    let lon = response.coord.lon;
+    let lat = response.coord.lat;
     
     // Get date and time and set the current date and time
-    var today = new Date();
-    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;
+    let today = new Date();
+    let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date+' '+time;
 
     // Displaying the date when the city is selected.
     $("#currentDate").text(dateTime);
     
     // Declaring the function F to C
-    var cTemp = fToC(response.main.temp);
+    let cTemp = fToC(response.main.temp);
     
     // Function to show Farenheit to Celcious
     function fToC(fahrenheit) {
@@ -55,24 +61,35 @@ $.ajax({
 
     // Formula for C
     //C = (5/9) * (F - 32)
-
-
 })
 };
 
 // 5 day forecast function 
 function forecast(city) {
-    
+    // Section for the 5 day query
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=3d16044a2eba4d271046d70fd1f2c155";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(forecastResponse){
-        console.log(forecastResponse);
+        // Creating  a filter
+        var filteredDays = forecastResponse.list.filter(
+            function (currentElement){
+            return currentElement.dt_txt.includes("12:00:00")
+            }	
+        );
+        console.log(filteredDays)
+        let content = $(".content")
+        for (i = 0; i < content.length; i++) {
+            let date = content[i].children("ul")[0].children("li").dates
+                
+        }
 
-    
+
     })
 };
+
+
 
 
 
